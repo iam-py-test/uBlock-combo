788d5c9e88a477eb87d868cee6eeb240af7b673d
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-09-05 10:22:13 -0400

    Add a few new lists

--- combo.py
+++ combo.py
@@ -1,6 +1,6 @@
 import requests
 import os
-lists = {"DandelionSprout Antimalware":"https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Dandelion%20Sprout's%20Anti-Malware%20List.txt","The malicious website blocklist":"https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/antimalware.txt"}
+lists = {"DandelionSprout Antimalware":"https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Dandelion%20Sprout's%20Anti-Malware%20List.txt","The malicious website blocklist":"https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/antimalware.txt","uBlock Filters":"https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt","Easylist":"https://easylist.to/easylist/easylist.txt","PUP Filter":"https://curben.gitlab.io/malware-filter/pup-filter.txt"}
 
 mainlist = """! Title: uBlock combo list
 ! Expires: 1 day
