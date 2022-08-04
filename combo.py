63849bb8bfbb2f2c359849bd36806ef4e306871e
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-08-04 08:29:44 -0400

    Licenses
    I never considered that I put a CC0 on this repo, but applied it to other people's lists which are under other licenses.

--- combo.py
+++ combo.py
@@ -1,11 +1,12 @@
 import requests
 import os
-lists = {"DandelionSprout Antimalware":"https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Dandelion%20Sprout's%20Anti-Malware%20List.txt","The malicious website blocklist":"https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/antimalware.txt","The anti-typo list":"https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/antitypo.txt","Actually Legitimate URL Shortener Tool":"https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt","uBlock Annoyances":"https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/annoyances.txt"}
+lists = {"Dandelion Sprout's Anti-Malware List":"https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Dandelion%20Sprout's%20Anti-Malware%20List.txt","The malicious website blocklist":"https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/antimalware.txt","The anti-typo list":"https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/antitypo.txt","Actually Legitimate URL Shortener Tool":"https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt"}
 
 mainlist = """! Title: uBlock combo list
 ! Expires: 1 day
 ! Homepage: https://github.com/iam-py-test/uBlock-combo
-
+! the Python script and my two lists are under CC0 
+! for Dandelion Sprout's Anti-Malware List and Actually Legitimate URL Shortener Tool, see https://github.com/DandelionSprout/adfilt/blob/master/LICENSE.md
 
 """
 
